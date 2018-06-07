fa7657a35225fa3edd8231dbf4c331f733be830c
fxghqc <konxiao@gmail.com>
2018-06-07 18:45:49 +0800

    it's a webpack plugin

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -113,6 +113,7 @@ module.exports = {
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
       new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+      new MonacoWebpackPlugin(),
     ],
   },
   module: {
@@ -182,7 +183,6 @@ module.exports = {
                   'import',
                   { libraryName: 'antd', libraryDirectory: 'es', style: 'css' },
                 ],
-                new MonacoWebpackPlugin(),
               ],
             },
           },
--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -119,6 +119,7 @@ module.exports = {
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
       new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+      new MonacoWebpackPlugin(),
     ],
   },
   module: {
@@ -185,7 +186,6 @@ module.exports = {
                   'import',
                   { libraryName: 'antd', libraryDirectory: 'es', style: 'css' },
                 ],
-                new MonacoWebpackPlugin(),
               ],
             },
           },
